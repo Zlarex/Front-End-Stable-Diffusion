a38228ff57e243a65f7fb2dd1014cc156cedc69b
Ihsan Fauzan <ihsanfauzan76@gmail.com>
2022-12-20 00:29:29 +0700

    ok

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import random
 app = Flask(__name__)
 
 def generateImgFromText(prompt, seed):
+    return "https://camo.githubusercontent.com/ec8cf13388b98a7133c6850afcf17ab4af5042802e0229c49f2242ea36d37090/68747470733a2f2f7261772e6769746875622e636f6d2f6d696b6f6c616c7973656e6b6f2f6c656e612f6d61737465722f6c656e612e706e67"
     return ""
 
 @app.route('/', methods=['GET'])
@@ -19,6 +20,8 @@ def generate():
         generated_count = formData['generated_count'] if formData['generated_count'] > 1 else 1
         is_random = bool(formData['random']) == True
         retVal = []
+
+        generated_count = generated_count if generated_count <= 4 else 4
 
         while(generated_count >= 1):
             # Todo processing
